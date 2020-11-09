925b86d96304ee7da9ec6de6c4466d1f79622523
srikanthpragada <srikanthpragada@gmail.com>
2020-11-09 17:56:09 +0530

    09-NOV

--- net_price.c
+++ net_price.c
@@ -6,9 +6,19 @@ void main()
 {
 float price,discount,net_price;
 
+  // Input
   printf("Enter price :");
   scanf("%f",&price);
+
+  // Calculation
   discount = 15 * price / 100;
   net_price = price - discount;
-  printf("Net price=%f",net_price);
+
+  // Output
+  printf("Price       : %10.2f\n",price);
+  printf("- Discount  : %10.2f\n",discount);
+  printf("              -----------\n");
+  printf("Net price   : %10.2f\n",net_price);
+  printf("              -----------\n");
+
 }
